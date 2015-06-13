e6c7301e6049e84eaa52c9ae64e1b23ccbb1b776
Gabriel <synthgab@users.noreply.github.com>
2015-06-13 10:50:09 +0200

    Update logic.js
    - Adding a few other tracking tokens I saw : aff_ , mtrck , xtor , xtref , xtrck
    - Adding the case of some sneaky web sites which don't use URL parameters but anchors (see example below):
    http://www.leparisien.fr/<category>/<article_title>.php#xtor=AD-1481423552&xtref=https%3A%2F%2Fwww.facebook.com%2F

--- logic.js
+++ logic.js
@@ -1,4 +1,3 @@
-
 function stripQuery(url) {
 	var remove = [
 		// Atlassian
@@ -18,12 +17,20 @@ function stripQuery(url) {
 
 		// Imdb
 		'ref_',
+
+		// Others
+		'aff_',
+		'mtrck',
+		'xtor',
+		'xtref',
+		'xtrck',
 	];
 
+	// Generic default case where we actually have a '?' for parameters in the URL
 	var q = url.split('?')[1];
 	if ( q ) {
 		q = '?' + q;
-		var regex = new RegExp('([\?\&](' + remove.join('|') + ')=[^&#]*)', 'ig');
+		var regex = new RegExp('([\?\&\#](' + remove.join('|') + ')=[^&#]*)', 'ig');
 		var params = [];
 		var filtered = q.replace(regex, '').replace(/^&/, '?');
 		if ( filtered != q ) {
@@ -31,6 +38,24 @@ function stripQuery(url) {
 				return filtered;
 			});
 			return nUrl;
+		}
+	} else {
+
+		// But some sites are sneaky and avoid the above by using anchors (e.g. #xtor=XXX')
+		// An example is the french news site leparisien.fr which uses this in its
+		// Facebook & Twitter links.
+		var r = url.split('#')[1];
+		if ( r ) {
+			r = '#' + r;
+			var regex = new RegExp('([\?\&\#](' + remove.join('|') + ')=[^&#]*)', 'ig');
+			var params = [];
+			var filtered = r.replace(regex, '');
+			if ( filtered != r ) {
+				var nUrl = url.replace(/\#.+/, function(m) {
+					return filtered;
+				});
+				return nUrl;
+			}
 		}
 	}
 }
