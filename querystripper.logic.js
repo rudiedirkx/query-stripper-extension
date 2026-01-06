6a3daed820201094d1ac6cf8d8ff2bf13afb21dc
Rudie Dirkx <github@hotblocks.nl>
2026-01-06 22:44:26 +0100

    queryTransform instead of regexSubstitution, no more replacement after hash/fragment

--- querystripper.logic.js
+++ querystripper.logic.js
@@ -33,36 +33,24 @@ const qs = {
 	async loadRules() {
 		const tokens = await qs.getTokens();
 
-		const groups = [''];
-		let group = 0;
-		for (let i = 0; i < tokens.length; i++) {
-			const token = tokens[i];
-			if (groups[group].length + 1 + token.length > qs.PATTERN_GROUP_MAX_LENGTH) {
-				groups.push('');
-				group++;
-			}
-
-			groups[group] += `|${token}`;
-		}
-
-		const rules = groups.map((group, i) => {
-			const regex = `([\\?\\#\\&])(${group.substr(1)})(=[^&#]*)?`;
-			return {
-				id: i + 1,
-				priority: 3,
-				action: {
-					type: 'redirect',
-					redirect: {
-						regexSubstitution: '\\1',
+		const rules = [{
+			id: 1,
+			priority: 3,
+			action: {
+				type: 'redirect',
+				redirect: {
+					regexSubstitution: '\\1',
+					transform: {
+						queryTransform: {
+							removeParams: tokens,
+						},
 					},
 				},
-				condition: {
-					regexFilter: regex,
-					isUrlFilterCaseSensitive: false,
-					resourceTypes: ['main_frame', 'sub_frame'],
-				},
-			};
-		});
+			},
+			condition: {
+				resourceTypes: ['main_frame', 'sub_frame'],
+			},
+		}];
 		await chrome.declarativeNetRequest.updateDynamicRules({
 			removeRuleIds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
 			addRules: rules,
